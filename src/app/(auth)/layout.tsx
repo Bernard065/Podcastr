4889a0b077630751922b5e63e0c8f2d6f4e0a5ff
Bernard065 <bebenibernard@gmail.com>
2024-09-20 22:35:01 +0300

    Create Clerk user and setup webhooks

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
 import React from "react";
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src="/assets/images/bg-img.png"
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      <div className="flex min-h-screen items-center justify-center">
+        {children}
+      </div>
+    </main>
+  );
 }
--- src/providers/ConvexClerkProvider.tsx
+++ src/providers/ConvexClerkProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
@@ -10,7 +10,27 @@ const convex = new ConvexReactClient(
 );
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+  <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    appearance={{
+      layout: {
+        socialButtonsVariant: "iconButton",
+        logoImageUrl: "/assets/icons/auth-logo.svg",
+      },
+      variables: {
+        colorBackground: "#15171c",
+        colorPrimary: "",
+        colorText: "white",
+        colorInputBackground: "#1b1f29",
+        colorInputText: "white",
+      },
+      elements: {
+        formButtonPrimary: "bg-orange-500 hover:bg-orange-400",
+        footerActionLink: "text-orange-500",
+        socialButtonsGitHub: "bg-white text-black hover:bg-gray-100",
+      },
+    }}
+  >
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
     </ConvexProviderWithClerk>
